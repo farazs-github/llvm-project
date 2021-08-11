502e5a69b3a0c5cf0902d1c07afe07bce4d8b864
Stefan Pejic <stefan.pejic@syrmia.com>
2021-08-11 10:23:08 +0200

    Properly handle long calls

--- llvm/lib/Target/Mips/MipsISelLowering.cpp
+++ llvm/lib/Target/Mips/MipsISelLowering.cpp
@@ -3567,7 +3567,9 @@ MipsTargetLowering::LowerCall(TargetLowering::CallLoweringInfo &CLI,
       if (Subtarget.useLongCalls())
         Callee = Subtarget.hasSym32()
                      ? getAddrNonPIC(N, SDLoc(N), Ty, DAG)
-                     : getAddrNonPICSym64(N, SDLoc(N), Ty, DAG);
+                     : Subtarget.hasNanoMips()
+                           ? getNMAddrNonPIC(N, SDLoc(N), Ty, DAG)
+                           : getAddrNonPICSym64(N, SDLoc(N), Ty, DAG);
     } else if (auto *N = dyn_cast<GlobalAddressSDNode>(Callee)) {
       bool UseLongCalls = Subtarget.useLongCalls();
       // If the function has long-call/far/near attribute
@@ -3581,7 +3583,9 @@ MipsTargetLowering::LowerCall(TargetLowering::CallLoweringInfo &CLI,
       if (UseLongCalls)
         Callee = Subtarget.hasSym32()
                      ? getAddrNonPIC(N, SDLoc(N), Ty, DAG)
-                     : getAddrNonPICSym64(N, SDLoc(N), Ty, DAG);
+                     : Subtarget.hasNanoMips()
+                           ? getNMAddrNonPIC(N, SDLoc(N), Ty, DAG)
+                           : getAddrNonPICSym64(N, SDLoc(N), Ty, DAG);
     }
   }
 
